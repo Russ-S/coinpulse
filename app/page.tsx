3fff7e9f66b0b95bd360d171a2f360241cff7b17
Russ Sweezeym <hesys@cs.com>
2026-06-18 16:31:36 -0600

    implement coins overview and trendsing coins us am functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,28 @@
-const Page = () => {
-  return <div></div>;
-};
-export default Page;
+import CoinsOverview from "@/components/home/CoinsOverview"
+import {
+  CoinOverviewFallback,
+  TrendingCoinsFallback,
+} from "@/components/home/fallback"
+import TrendingCoins from "@/components/home/TrendingCoins"
+import { Suspense } from "react"
+
+const Page = async () => {
+  return (
+    <main className="main-container">
+      <section className="home-grid">
+        <Suspense fallback={<CoinOverviewFallback />}>
+          <CoinsOverview />
+        </Suspense>
+
+        <Suspense fallback={<TrendingCoinsFallback />}>
+          <TrendingCoins />
+        </Suspense>
+      </section>
+
+      <section className="w-full mt-7 space-y-4">
+        <p>Categories</p>
+      </section>
+    </main>
+  )
+}
+export default Page
