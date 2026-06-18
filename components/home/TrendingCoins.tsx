ca0f09e9f94a2376a5349782d9aa4613fc8bcda8
Russ Sweezeym <hesys@cs.com>
2026-06-18 16:54:05 -0600

    fix critical issues

--- components/home/TrendingCoins.tsx
+++ components/home/TrendingCoins.tsx
@@ -1,61 +1,73 @@
-import { fetcher } from '@/lib/coingecko.actions';
-import Link from 'next/link';
-import Image from 'next/image';
-import { cn, formatCurrency } from '@/lib/utils';
-import { TrendingDown, TrendingUp } from 'lucide-react';
-import DataTable from '@/components/DataTable';
+import { fetcher } from "@/lib/coingecko.actions"
+import Link from "next/link"
+import Image from "next/image"
+import { cn, formatCurrency } from "@/lib/utils"
+import { TrendingDown, TrendingUp } from "lucide-react"
+import DataTable from "@/components/DataTable"
+import { TrendingCoinsFallback } from "./fallback"
 // import { TrendingCoinsFallback } from './fallback';
 
 const TrendingCoins = async () => {
-  const trendingCoins = await fetcher<{ coins: TrendingCoin[] }>('/search/trending', undefined, 300);
+  let trendingCoins
+
+  try {
+    trendingCoins = await fetcher<{ coins: TrendingCoin[] }>(
+      "/search/trending",
+      undefined,
+      300,
+    )
+  } catch (error) {
+    console.error("Error fetching trending coins:", error)
+    return <TrendingCoinsFallback />
+  }
 
   const columns: DataTableColumn<TrendingCoin>[] = [
     {
-      header: 'Name',
-      cellClassName: 'name-cell',
+      header: "Name",
+      cellClassName: "name-cell",
       cell: (coin) => {
-        const item = coin.item;
+        const item = coin.item
 
         return (
           <Link href={`/coins/${item.id}`}>
             <Image src={item.large} alt={item.name} width={36} height={36} />
             <p>{item.name}</p>
           </Link>
-        );
+        )
       },
     },
     {
-      header: '24h Change',
-      cellClassName: 'change-cell',
+      header: "24h Change",
+      cellClassName: "change-cell",
       cell: (coin) => {
-        const item = coin.item;
-        const isTrendingUp = item.data.price_change_percentage_24h.usd > 0;
+        const item = coin.item
+        const isTrendingUp = item.data.price_change_percentage_24h.usd > 0
 
         return (
           <div
-          className={cn(
-            "price-change",
-            isTrendingUp ? "text-green-500" : "text-red-500",
-          )}
-        >
-          <p>
-            {isTrendingUp ? (
-              <TrendingUp width={16} height={16} />
-            ) : (
-              <TrendingDown width={16} height={16} />
+            className={cn(
+              "price-change",
+              isTrendingUp ? "text-green-500" : "text-red-500",
             )}
-            {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
-          </p>
-        </div>
-        );
+          >
+            <p>
+              {isTrendingUp ? (
+                <TrendingUp width={16} height={16} />
+              ) : (
+                <TrendingDown width={16} height={16} />
+              )}
+              {Math.abs(item.data.price_change_percentage_24h.usd).toFixed(2)}%
+            </p>
+          </div>
+        )
       },
     },
     {
-      header: 'Price',
-      cellClassName: 'price-cell',
+      header: "Price",
+      cellClassName: "price-cell",
       cell: (coin) => formatCurrency(coin.item.data.price),
     },
-  ];
+  ]
 
   return (
     <div id="trending-coins">
@@ -70,7 +82,7 @@ const TrendingCoins = async () => {
         bodyCellClassName="py-2!"
       />
     </div>
-  );
-};
+  )
+}
 
-export default TrendingCoins;
+export default TrendingCoins
